95b33831e9fb30b691caa9c0502883f0ebae8140
D-future0 <oka4collins@gmail.com>
2024-08-04 00:05:05 +0100

    update favourite meal

--- recipes_app/app.js
+++ recipes_app/app.js
@@ -1,4 +1,5 @@
-let favArray = [];
+const favMealContainer = document.querySelector(`.favMeal-container`)
+const randomMeal = document.querySelector(`.random-meal`)
 function randomMeals() {
   async function dailyBread2() {
     const resp = await fetch(
@@ -11,10 +12,10 @@ function randomMeals() {
     let displaymeal = meals.map((meal) => {
       return `<img src="${meal.strMealThumb}" />
             <div class="meal">
-              <p>${meal.strMeal}</p><span id="${meal.idMeal}" class="addToFav">love</span>
+              <p>${meal.strMeal}</p><button type="submit" id="${meal.idMeal}" class="addToFav">love</button>
             </div>`;
     });
-    document.querySelector(`.random-meal`).innerHTML = displaymeal;
+    randomMeal.innerHTML = displaymeal;
     const selectFav = document.querySelector(".addToFav");
     selectFav.addEventListener(`click`, (e) => {
       const id = e.target.id;
@@ -31,8 +32,8 @@ function randomMeals() {
   }
   dailyBread2();
 }
-randomMeals();
 
+// local storage functionalities
 const addFavToLocalStorage = (id) => {
 localStorage.setItem("mealIds", JSON.stringify([...getFavFromLocalStorage(), id]));
 }
@@ -43,5 +44,41 @@ const getFavFromLocalStorage = () => {
 const removeFromLocalstorage = (mealsId)=>{
     localStorage.setItem("mealIds", JSON.stringify(getFavFromLocalStorage().filter((id)=> id !== mealsId)));
 }
-// console.log(favMealContainer)
-// searchmeal()
+// add to favourite meal
+async function getMealsById(id){
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+  );
+  const data = await resp.json();
+  const meals = data.meals[0];
+  console.log(meals)
+  return meals
+}
+
+async function updateFavmeals (){
+let mealIds = getFavFromLocalStorage()
+// let favMeals = []
+for (let index = 0; index < mealIds.length; index++) {
+  let mealId = mealIds[index];
+  const meals = await getMealsById(mealId)
+  // favMeals.push(meals)
+  // console.log(meals)
+  displayFav(meals)
+}
+
+}
+const displayFav  = (meals)=>{
+  const favMeal = document.createElement(`li`)
+  favMeal.innerHTML =  `<li class="my-fav">
+            <img src="${meals.strMealThumb}" alt="favorite meal" />
+            <p>${meals.strMeal}</p>
+          </li>`;
+  favMealContainer.appendChild(favMeal)
+
+}
+function setupApp(){
+   randomMeals();
+   updateFavmeals()
+}
+
+setupApp()
