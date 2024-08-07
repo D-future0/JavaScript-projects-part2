dd640e214e9b452c23bde734f057c2605a16c3aa
D-future0 <oka4collins@gmail.com>
2024-08-07 14:39:58 +0100

    note app setup

--- note_app/index.js
+++ note_app/index.js
@@ -1,3 +1,19 @@
+const addNote = document.querySelector(`#add-note`)
+const noteCenter = document.querySelector(`.note-center`)
+
+console.log(addNote)
+addNote.addEventListener(`click`, ()=>{
+    const div = document.createElement(`div`)
+    div.classList.add(`note`)
+    div.innerHTML = `<div class="tool-bar">
+    <button class="edit">edit</button>
+    <button class="trash">trash</button>
+</div>
+<div class="main"></div>
+<textarea name="text" class="text hide"></textarea>`;
+
+noteCenter.appendChild(div)
+
 const edit = document.querySelector(`.edit`)
 const trash = document.querySelector(`.trash`)
 const main = document.querySelector(`.main`)
@@ -7,8 +23,14 @@ edit.addEventListener(`click`, ()=>{
     main.classList.toggle(`hide`)
     text.classList.toggle(`hide`)
 })
+trash.addEventListener(`click`, (e)=>{
+    // const trashNote = e.target.parentElement.parentElement
+    // note.remove()
+})
 text.addEventListener(`input`, (e)=>{
-const{ value } = e.target.value
+const{ value } = e.target
 console.log(value)
-main.innerHTML= marked(value)
+main.innerHTML = `${marked.parse(value)}`
+})
+
 })
